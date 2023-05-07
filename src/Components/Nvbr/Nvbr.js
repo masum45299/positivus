d6703717311ce68140468c050ae32c5950530135
masum45299 <masum459299@gmail.com>
2023-05-07 15:33:59 +0600

    first commit

---
+++ src/Components/Nvbr/Nvbr.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+
+const Nvbr = () => {
+    return (
+        <div>
+            <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">React Bootstrap Navbar</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
+                    <Nav.Link href="#about">About</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Nvbr;
