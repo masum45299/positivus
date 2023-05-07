11dde1faa9b1e933db5dfe0d006de7a67d3fc919
masum45299 <masum459299@gmail.com>
2023-05-07 21:11:16 +0600

    navBar

--- src/Components/Nvbr/Nvbr.js
+++ src/Components/Nvbr/Nvbr.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from '../../Img/Logo.png'
 
 
 const Nvbr = () => {
@@ -7,11 +8,11 @@ const Nvbr = () => {
         <div>
             <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React Bootstrap Navbar</Navbar.Brand>
+                <Navbar.Brand href="#home"> <img src={logo} alt=''/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#home">About Us</Nav.Link>
                     <Nav.Link href="#link">Link</Nav.Link>
                     <Nav.Link href="#about">About</Nav.Link>
                 </Nav>
