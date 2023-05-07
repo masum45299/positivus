c8f92dcda97e7a62c46bae5d3271ca84a2d5664d
masum45299 <masum459299@gmail.com>
2023-05-08 00:26:50 +0600

    resposive Nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Nvbr from './Components/Nvbr/Nvbr';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 function App() {
+
+  const router= createBrowserRouter([
+    {path:"/", element:<Nvbr/>},
+    {path:"about", element:<Nvbr/>},
+    {path:"services", element:<Nvbr/>}
+  ])
+
+
   return (
     <div className="App">
-      <Nvbr/>
+      <RouterProvider
+      router={router}
+      />
+      
     </div>
   );
 }
--- src/Components/Nvbr/Nvbr.js
+++ src/Components/Nvbr/Nvbr.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import logo from '../../Img/Logo.png'
+import { Navbar, Nav, Container,NavDropdown, NavLink } from 'react-bootstrap';
+import logo from '../../Img/Logo.png';
+import './Nvbr.css'
+import { Link } from 'react-router-dom';
 
 
 const Nvbr = () => {
     return (
-        <div>
-            <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home"> <img src={logo} alt=''/></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link href="#home">About Us</Nav.Link>
-                    <Nav.Link href="#link">Link</Nav.Link>
-                    <Nav.Link href="#about">About</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-            </Navbar>
+        <div className='Nvbr'>
+    <Navbar collapseOnSelect expand="lg" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home"><img src={logo} alt=''/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/about">About us</Nav.Link>
+            <Nav.Link as={Link} to="/services">Services</Nav.Link>
+            <Nav.Link as={Link} to="about"  >Use Cases</Nav.Link>
+            <Nav.Link as={Link} to="about" >Pricing</Nav.Link>
+            <Nav.Link as={Link} to="about"  >Blog</Nav.Link>
+            <Nav.Link as={Link} to="about">Request a quote</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
         </div>
     );
 };
