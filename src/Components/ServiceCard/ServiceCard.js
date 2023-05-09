af8e5bd4e133d660c1912819a7271548c1b14d47
masum45299 <masum459299@gmail.com>
2023-05-09 18:25:12 +0600

    Service_card

--- src/Components/ServiceCard/ServiceCard.js
+++ src/Components/ServiceCard/ServiceCard.js
@@ -1,53 +1,81 @@
 import React from 'react';
 import './ServiceCard.css';
 import { UilExternalLinkAlt } from '@iconscout/react-unicons';
-import magnifiyer from '../../Img/magnifiyer.png'
+import magnifiyer from '../../Img/magnifiyer.png';
+import illustration from '../../Img/Illustration.png';
+import love from '../../Img/love.png';
+import sending from '../../Img/sending.png';
+import illustration2 from '../../Img/illustration2.png';
+import volumic from '../../Img/vlomuemic.png';
 
 const ServiceCard = () => {
     return (
         <div className='s-card-wrap'>
-            <div className='S-card'>
+            <div className='S-card S-card1'>
                 <div className='card-content'>
                     <h3 style={{backgroundColor:"#B9FF66", marginBottom:"11rem"}}>Search engine <br/> Optimization</h3>
-                    <UilExternalLinkAlt/>
+                    <div className='i-flex'>
+                        <div className='s-icon icon1'><UilExternalLinkAlt/></div>
+                        <small>Learn more</small>
+                    </div>
                 </div>
                 <img src={magnifiyer} alt=''/>
             </div>
-            <div className='S-card'>
+            <div className='S-card S-card2'>
+                <div className='card-content'>
+                    <h3 style={{backgroundColor:"#ffffff", marginBottom:"11rem"}}>Pay-per-click <br/> advertising</h3>
+                    <div className='i-flex'>
+                        <div className='s-icon icon2'><UilExternalLinkAlt/></div>
+                        <small>Learn more</small>
+                    </div>
+                </div>
+                <img src={illustration} alt=''/>
+            </div>
+            <div className='S-card S-card3'>
+                <div className='card-content'>
+                    <h3 style={{backgroundColor:"#ffffff", marginBottom:"11rem"}}>Social Media<br/> Marketing</h3>
+                    <div className='i-flex'>
+                        <div className='s-icon icon3'><UilExternalLinkAlt/></div>
+                        <small style={{color:"#ffffff"}}>Learn more</small>
+                    </div>
+                </div>
+                <img src={love} alt=''/>
+            </div>
+            <div className='S-card S-card4'>
                 <div className='card-content'>
                     <h3 style={{backgroundColor:"#B9FF66", marginBottom:"11rem"}}>Search engine <br/> Optimization</h3>
-                    <UilExternalLinkAlt/>
+                    <div className='i-flex'>
+                        <div className='s-icon icon1'><UilExternalLinkAlt/></div>
+                        <small>Learn more</small>
+                    </div>
                 </div>
-                <img src={magnifiyer} alt=''/>
+                <img src={sending} alt=''/>
             </div>
-            <div className='S-card'>
+            <div className='S-card S-card5'>
                 <div className='card-content'>
-                    <h3 style={{backgroundColor:"#B9FF66", marginBottom:"11rem"}}>Search engine <br/> Optimization</h3>
-                    <UilExternalLinkAlt/>
+                    <h3 style={{backgroundColor:"#ffffff", marginBottom:"11rem"}}>Search engine <br/> Optimization</h3>
+                    <div className='i-flex'>
+                        <div className='s-icon icon1'><UilExternalLinkAlt/></div>
+                        <small>Learn more</small>
+                    </div>
                 </div>
-                <img src={magnifiyer} alt=''/>
+                <img src={illustration2} alt=''/>
             </div>
-            <div className='S-card'>
+            <div className='S-card S-card6'>
                 <div className='card-content'>
-                    <h3 style={{backgroundColor:"#B9FF66", marginBottom:"11rem"}}>Search engine <br/> Optimization</h3>
-                    <UilExternalLinkAlt/>
+                    <h3 style={{backgroundColor:"##B9FF66", marginBottom:"11rem"}}>Search engine <br/> Optimization</h3>
+                    <div className='i-flex'>
+                        <div className='s-icon icon6'><UilExternalLinkAlt/></div>
+                        <small style={{color:"#ffffff"}}>Learn more</small>
+                    </div>
                 </div>
-                <img src={magnifiyer} alt=''/>
+                <img src={volumic} alt=''/>
             </div>
-            <div className='S-card'>
-                <div className='card-content'>
-                    <h3 style={{backgroundColor:"#B9FF66", marginBottom:"11rem"}}>Search engine <br/> Optimization</h3>
-                    <UilExternalLinkAlt/>
-                </div>
-                <img src={magnifiyer} alt=''/>
-            </div>
-            <div className='S-card'>
-                <div className='card-content'>
-                    <h3 style={{backgroundColor:"#B9FF66", marginBottom:"11rem"}}>Search engine <br/> Optimization</h3>
-                    <UilExternalLinkAlt/>
-                </div>
-                <img src={magnifiyer} alt=''/>
-            </div>
+         
+        
+         
+          
+            
         </div>
     );
 };
