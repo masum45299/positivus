8b4c4f2c5bd0b506df7f460c02d50fe4fe8a94cf
masum45299 <masum459299@gmail.com>
2023-05-10 18:13:37 +0600

    swiper

--- src/Components/Case/Case.js
+++ src/Components/Case/Case.js
@@ -5,17 +5,42 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { UilExternalLinkAlt } from '@iconscout/react-unicons';
 import 'swiper/css/bundle';
 
+
+
+
+
 const Case = () => {
+ 
+
     return (
-        <div>
+        
+        <div className='case'>
             <Container>
                 <div className='S-content-f'>
                     <button className='m-btn s-btn'>Case Studies</button>
                     <p style={{marginLeft:"3rem", fontSize:"1.6rem"}}>Explore Real-Life Examples of Our Proven Digital Marketing <br/> Success through Our Case Studies</p>
                 </div>
+                <div className='desktop'>
                 <Swiper
                 slidesPerView={3}
+                className='slider-side'
                 >
+                    <SwiperSlide>
+                        <p style={{fontSize:"1.6rem", color:"#ffffff"}}>For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.</p>
+                        <div className='swiper-icon'>
+                            <p style={{marginRight:"1.5rem", marginBottom:"0rem", fontSize:"1.6rem", color:"#B9FF66"}}>Learn more</p>
+                            <UilExternalLinkAlt/>
+                        </div>
+                        <div className='after'></div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <p style={{fontSize:"1.6rem", color:"#ffffff"}}>For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.</p>
+                        <div className='swiper-icon'>
+                            <p style={{marginRight:"1.5rem", marginBottom:"0rem", fontSize:"1.6rem", color:"#B9FF66"}}>Learn more</p>
+                            <UilExternalLinkAlt/>
+                        </div>
+                        <div className='after'></div>
+                    </SwiperSlide>
                     <SwiperSlide>
                         <p style={{fontSize:"1.6rem", color:"#ffffff"}}>For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.</p>
                         <div className='swiper-icon'>
@@ -24,6 +49,38 @@ const Case = () => {
                         </div>
                     </SwiperSlide>
                 </Swiper>
+                </div>
+                
+                <div className='mobile'>
+                <Swiper
+                slidesPerView={1}
+                className='slider-side'
+                >
+                    <SwiperSlide>
+                        <p style={{fontSize:"1.6rem", color:"#ffffff"}}>For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.</p>
+                        <div className='swiper-icon'>
+                            <p style={{marginRight:"1.5rem", marginBottom:"0rem", fontSize:"1.6rem", color:"#B9FF66"}}>Learn more</p>
+                            <UilExternalLinkAlt/>
+                        </div>
+                        <div className='after'></div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <p style={{fontSize:"1.6rem", color:"#ffffff"}}>For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.</p>
+                        <div className='swiper-icon'>
+                            <p style={{marginRight:"1.5rem", marginBottom:"0rem", fontSize:"1.6rem", color:"#B9FF66"}}>Learn more</p>
+                            <UilExternalLinkAlt/>
+                        </div>
+                        <div className='after'></div>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <p style={{fontSize:"1.6rem", color:"#ffffff"}}>For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.</p>
+                        <div className='swiper-icon'>
+                            <p style={{marginRight:"1.5rem", marginBottom:"0rem", fontSize:"1.6rem", color:"#B9FF66"}}>Learn more</p>
+                            <UilExternalLinkAlt/>
+                        </div>
+                    </SwiperSlide>
+                </Swiper>
+                </div>
             </Container>
         </div>
     );
